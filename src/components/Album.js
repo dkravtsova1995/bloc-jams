a36cef67d784f942e9b1b8ae48a5476bd1b15498
Daria Kravtsova <kravcova.dasha@bk.ru>
2019-03-14 16:04:04 -0600

    Added audio to Album.js.

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,9 +11,39 @@ const album = albumData.find( album => {
     });
 
   this.state = {
-     album: album
+    album: album,
+    currentSong: album.songs[0],
+    isPlaying: false
   };
+
+  this.audioElement = document.createElement('audio');
+  this.audioElement.src = album.songs[0].audioSrc;
 }
+
+  play() {
+     this.audioElement.play();
+     this.setState({ isPlaying: true });
+   }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+     this.audioElement.src = song.audioSrc;
+     this.setState({ currentSong: song });
+   }
+
+   handleSongClick(song) {
+     const isSameSong = this.state.currentSong === song;
+     if (this.state.isPlaying && isSameSong) {
+       this.pause();
+     } else {
+       if (!isSameSong) { this.setSong(song); } 
+       this.play();
+     }
+   }
 
 render() {
     return (
@@ -35,7 +65,7 @@ render() {
            <tbody>
              {
                this.state.album.songs.map( (song, index) =>
-                 <tr className="song" key={index} >
+                 <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
                    <td className="song-number">{index+1}</td>
                    <td className="song-title">{song.title}</td>
                    <td className="song-duration">{this.formatTime(song.duration)}</td>
