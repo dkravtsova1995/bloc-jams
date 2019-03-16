3210981373d740370124e491f587557fa9180e45
Daria Kravtsova <kravcova.dasha@bk.ru>
2019-03-15 20:57:15 -0600

    Added previous button.

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props) {
@@ -44,6 +45,14 @@ const album = albumData.find( album => {
      }
    }
 
+   handlePrevClick() {
+     const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+     const newIndex = Math.max(0, currentIndex - 1);
+     const newSong = this.state.album.songs[newIndex];
+     this.setSong(newSong);
+     this.play();
+   }
+
 render() {
     return (
       <section className="album">
@@ -77,7 +86,13 @@ render() {
                )}
            </tbody>
          </table>
-      </section>
+         <PlayerBar
+             isPlaying={this.state.isPlaying}
+             currentSong={this.state.currentSong}
+             handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+             handlePrevClick={() => this.handlePrevClick()}
+           />
+    </section>
     );
   }
 }
