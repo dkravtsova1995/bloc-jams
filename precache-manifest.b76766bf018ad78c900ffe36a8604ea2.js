34a4621cabbc4f16b1b00ff6766ccb3957f6e910
Daria Kravtsova <kravcova.dasha@bk.ru>
2020-02-03 23:56:45 -0700

    Updates

---
+++ precache-manifest.b76766bf018ad78c900ffe36a8604ea2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bd6e2627f67b411d29c",
+    "url": "/bloc-jams/static/js/runtime~main.34f7fe66.js"
+  },
+  {
+    "revision": "bba1cc82a9326e816d12",
+    "url": "/bloc-jams/static/js/main.361feac9.chunk.js"
+  },
+  {
+    "revision": "66c51c164a081bf00e7a",
+    "url": "/bloc-jams/static/js/2.9ed52c5b.chunk.js"
+  },
+  {
+    "revision": "bba1cc82a9326e816d12",
+    "url": "/bloc-jams/static/css/main.cbb6ff89.chunk.css"
+  },
+  {
+    "revision": "843d0bb5ddeed3b6bd5907cf24353f76",
+    "url": "/bloc-jams/index.html"
+  }
+];
